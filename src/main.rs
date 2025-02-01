20a0ac4e96153b0fc7fbae4d0261592d5189359b
fcelli <fedecel93@gmail.com>
2025-02-01 17:03:06 +0000

    feat: fetch and save ndc product data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+mod database;
+mod models;
+
+use api::ndc::fetch_and_save;
+use database::get_db_connection;
+use dotenv::dotenv;
+use std::env;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set.");
+
+    let pool = get_db_connection(&db_url)
+        .await
+        .expect("Database could not be initialised.");
+
+    fetch_and_save(&pool)
+        .await
+        .expect("An error occurred while fetching NDC data.");
 }
